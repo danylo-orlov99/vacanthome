8f453bb112406af725d5dfdb455ae035e5620629
Danylo Orlov <dorlov.nycc@gmail.com>
2025-03-03 14:37:00 -0500

    C2

--- code/00_load_dependencies.R
+++ code/00_load_dependencies.R
@@ -2,25 +2,22 @@
 
 #' NOTE: The code below is intended to load all listed libraries. If you do not
 #' have these libraries on your computer, the code will attempt to INSTALL them.
-#' 
+#'
 #' IF YOU DO NOT WANT TO INSTALL ANY OF THESE PACKAGES, DO NOT RUN THIS CODE.
 
-list.of.packages <- c("tidyverse", "janitor")
+list.of.packages <- c("knitr", "dplyr", "flextable")
 
-# checks if packages has been previously installed
+# Check if packages have been previously installed
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-#councildown.check <- "councildown" %in% installed.packages()[,"Package"]
-councilverse.check <- "councilverse" %in% installed.packages()[,"Package"]
 
-# if not, packages are installed
+# If not, install the packages
 if(length(new.packages)) install.packages(new.packages)
-#if(councildown.check == FALSE) remotes::install_github("newyorkcitycouncil/councildown")
-if(councilverse.check == FALSE) remotes::install_github("newyorkcitycouncil/councilverse")
-  
-# packages are loaded
-lapply(c(list.of.packages,"councilverse"), require, character.only = TRUE)
 
-# remove created variables for packages
-rm(list.of.packages,new.packages,councilverse.check)
+# Load the packages
+lapply(list.of.packages, require, character.only = TRUE)
+
+# Remove created variables for packages
+rm(list.of.packages, new.packages)
+
 
 ## Functions -----------------------------------------------
